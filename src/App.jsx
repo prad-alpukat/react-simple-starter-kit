cdcd12df8d8fcacfc1b2dcd9870111e87faa703e
praditya aldi syahputra <pradityaaldi.s@gmail.com>
2024-01-25 12:23:29 +0700

    installed react, react-router-dom, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import pages
+import Home from './pages/Home';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
